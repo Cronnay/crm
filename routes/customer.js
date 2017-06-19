a5c2cb65c78b0b9312fcd21f205b833b178ccf32
Cronnay <cronnay@hotmail.com>
2017-06-19 18:36:14 +0200

    Remove a default user

--- routes/customer.js
+++ routes/customer.js
@@ -5,8 +5,6 @@ var User = require("../models/user");
 var Customer = require("../models/customer");
 var middleware = require("../middleware");
 
-var newCustomer = {namn:"Alfred AB", orgid:5566778899, adress:"Timjanvägen 10", postnr: "96164", postort: "Boden", telefonnummer: "0722003579", fornamn:"Sebbe", efternamn:"Berglönn", status:"Inte uppringd"};
-
 router.get("/secret", middleware.isLoggedIn, function(req,res){
     User.findById(req.user._id).populate("customer").exec(function(err, user){
         res.render("secret", {data: user});
