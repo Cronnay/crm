8922300fc1f9ceaa58581411a3906bdd6f6384f6
Cronnay <cronnay@hotmail.com>
2017-06-20 15:24:10 +0200

    When uploading excelfile, it will now save to the database

--- routes/customer.js
+++ routes/customer.js
@@ -58,10 +58,45 @@ router.post("/secret-files", middleware.isLoggedIn, type, function(req,res, next
         var first_sheet_name = workbook.SheetNames[0]; //första arketnamnet
         var worksheet = workbook.Sheets[first_sheet_name]; //välj arket med arknamnet
         var obj = xlsx.utils.sheet_to_json(worksheet);  //skriver ut det som en json-sträng.
-        fs.unlink("/uploads" + req.file.path, function(err){ //Tar bort loggen som sparas
+        /*fs.unlink("./uploads" + req.file.path, function(err){ //Tar bort loggen som sparas
             if(err){
                 console.log(err);
             }
+        });*/
+        obj.forEach(function(customer){
+            var newCustomer = {
+                "orgid" : customer.Organisationsnummer, 
+                "foretagsnamn" : customer.Företagsnamn, 
+                "adress" : customer.Besöksadress, 
+                "postnr": customer.Postnummer,
+                "postort" : customer.Postort,
+                "kontaktnamn": customer.Kontaktnamn,
+                "kontakttelefon": customer.Kontakt_Telefon,
+                "kontaktepost" : customer.Kontakt_Epost,
+                "kontakthemsida": customer.Kontakt_Hemsida,
+                "antalanstallda" : customer.Antal_Anställda,
+                "juridiskform": customer.Juridisk_form,
+                "koncernmoderbolagsnamn" : customer.Koncermoderbolagsnamn,
+                "nettoomsattning": customer.Nettoomsättning,
+                "vd": customer.VD
+            }
+            User.findById(req.user._id, function(err, user){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    Customer.create(newCustomer, function(error, newcustomer){
+                        if(error){
+                            console.log(error);
+                        }
+                        else{
+                            user.customer.push(newcustomer);
+                            user.save();
+                            console.log(newcustomer);
+                        }
+                    })
+                }
+            });
         });
         res.render("test.ejs", {data : obj})
     
